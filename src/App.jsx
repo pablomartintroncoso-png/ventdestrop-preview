45c9de06d6c0311b40382569ef5b7362571ceb8d
pablomartintroncoso-png <pablomartintroncoso@gmail.com>
2025-11-13 14:59:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -169,8 +169,6 @@ export default function App() {
             ))}
           </nav>
 
-          {/* Actions */}
- {/* Actions */}
 
 {/* Actions */}
 <a
@@ -182,16 +180,6 @@ export default function App() {
 >
   Fes-te soci
 </a>
-
-  <a
-    href="https://app.cluber.es/clubes/68ff44c49f856547379716/inscripcion"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Fes-te soci
-  </a>
-</Button>
-
 
             <Button variant="outline" className="border-slate-300 hidden md:inline-flex" onClick={() => setLang(lang === "CAT" ? "ES" : "CAT")}>
               <Globe className="h-4 w-4 mr-1"/>{lang}
